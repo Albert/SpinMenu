6ebad058f55fe13a1f585e7ec4137e442fa94ed3
Albert Hwang <hwang.al@gmail.com>
2016-05-26 12:38:24 -0400

    spin menu

---
+++ Assets/SpinToMouse.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinToMouse : MonoBehaviour {
+	Vector3 lastMousePosition;
+	void Start () {
+	}
+	
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			lastMousePosition = Input.mousePosition;
+		}
+
+		//freeRotate ();
+		constrainedRotate();
+
+	}
+
+	void freeRotate() {
+		if (Input.GetMouseButton (0)) {
+			Vector3 deltaVector = Input.mousePosition - lastMousePosition;
+			Vector3 rotationAxis = Vector3.Cross(deltaVector, Vector3.forward);
+			transform.RotateAround (transform.position, rotationAxis, deltaVector.magnitude);
+			lastMousePosition = Input.mousePosition;
+		}
+	}
+
+	void constrainedRotate() {
+		if (Input.GetMouseButton (0)) {
+			Vector3 deltaVector = Input.mousePosition - lastMousePosition;
+			deltaVector.x = 0;
+			Vector3 rotationAxis = Vector3.Cross(deltaVector, Vector3.forward);
+			transform.RotateAround (transform.position, rotationAxis, deltaVector.magnitude);
+			lastMousePosition = Input.mousePosition;
+		}
+	}
+}
